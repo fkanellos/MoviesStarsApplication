d80a26c6692d47a8a083553b680821f2d65a96a1
PaprikaSol <zoipapajim@gmail.com>
2021-02-23 20:17:24 +0200

    v0.4

---
+++ app/src/main/java/com/example/moviestarsapp/PopularDetails.java
@@ -0,0 +1,22 @@
+package com.example.moviestarsapp;
+
+public class PopularDetails {
+
+    private String poster_path;
+    private Boolean adult;
+    private String overview;
+    private String release_date;
+    private int[] genre_ids;
+    private int id;
+    private String original_title;
+    private String title;
+    private String backdrop_path;
+    private String original_language;
+    private double popularity;
+    private int vote_count;
+    private Boolean video;
+    private double vote_average;
+
+
+    }
+
---
+++ app/src/main/java/com/example/moviestarsapp/PopularResponse.java
@@ -0,0 +1,12 @@
+package com.example.moviestarsapp;
+
+import java.util.List;
+
+public class PopularResponse {
+
+    private int page;
+    private List<PopularDetails> results;
+    private int total_results;
+    private int total_pages;
+
+}
