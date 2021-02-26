08d705f3493bfb4be7e31229c4523982cd013559
PaprikaSol <zoipapajim@gmail.com>
2021-02-27 01:55:42 +0200

    2.5.1 before merge

--- app/src/main/java/com/example/moviestarsapp/register/RegisterActivity.java
+++ app/src/main/java/com/example/moviestarsapp/register/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.moviestarsapp.register;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +16,14 @@ public class RegisterActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        EditText email = findViewById(R.id.email_edit);
+        EditText user = findViewById(R.id.username_edit);
+
     }
 }
